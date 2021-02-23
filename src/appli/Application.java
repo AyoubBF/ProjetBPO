e8946d41befc8ec2f2de6e24815e246ce8cb0989
AyoubBF <72936702+AyoubBF@users.noreply.github.com>
2021-02-23 15:21:52 +0100

    Début
    class joueur + méthode de création des cartes

---
+++ src/appli/Application.java
@@ -0,0 +1,12 @@
+package appli;
+
+public class Application {
+    public static void main(String[] args) {
+        Joueur NORD = new Joueur();
+        Joueur SUD = new Joueur();
+        String[] DeckNORD = new String[60];
+        String[] DeckSUD = new String[60];
+        NORD.creerCartes(DeckNORD);
+        SUD.creerCartes(DeckSUD);
+    }
+}
